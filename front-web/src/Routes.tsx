3f1186653cd70590ac38f5344cfc5140284b3779
Vanderson Nery <vandersonrn@gmail.com>
2020-09-20 23:47:03 -0300

    Criando página de listagem dos registros

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -11,7 +11,7 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="records" exact>
+            <Route path="/records" exact>
                 <Records />
             </Route>
         </Switch>
